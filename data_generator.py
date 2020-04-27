7f56859dc2369ce4279bf00e236bb15718159743
Klim <klimmy@MacBook-Pro-Kliment.local>
2020-04-27 18:15:34 +0300

    completely changed algorithm to find extrema

--- data_generator.py
+++ data_generator.py
@@ -24,7 +24,7 @@ def parse_arguments():
                         help='T, multiplier for extremum constraint. The bigger T the less extrema will be found',
                         type=float,
                         nargs='?',
-                        default=1.1)
+                        default=5.7)
     parser.add_argument('-k',
                         help='k, extremum constraint. k + 1 = minimum points between two extrema',
                         type=int,
@@ -37,7 +37,7 @@ def parse_arguments():
     return parser.parse_args()
 
 
-class syntheticSeries:
+class SyntheticSeries:
     """
     Generates a synthetic time series X of predefined length N * M
     Calculates all extrema (YMin, YMax) of this time series with the following constraints:
@@ -56,9 +56,10 @@ class syntheticSeries:
         self.M = M
         self.k = k
         self.X = np.empty((self.N * self.M))
+        self.length = (self.N * self.M) - 1
         self.D = None
-        self.YMin = None
-        self.YMax = None
+        self.YMin = np.array([0 for i in range(self.length + 1)])
+        self.YMax = np.array([0 for i in range(self.length + 1)])
 
     def generate(self, seed=None):
         """
@@ -76,42 +77,101 @@ class syntheticSeries:
         Calculates extrema with described constraints for generated time series X
         :param T: Multiplier for the second extremum constraint. The bigger T the less extrema will be found
         """
+        self.YMin = np.array([0 for i in range(self.length + 1)])
+        self.YMax = np.array([0 for i in range(self.length + 1)])
         self.D = T * np.std(self.X[:-self.k] - self.X[self.k:])
-        extrema = self._get_extrema()
-        extrema_indexes = extrema.nonzero()[0]
-        extrema_indexes = self._apply_idx_constraint(extrema_indexes)
-        extrema_indexes = self._apply_value_constraint(extrema_indexes)
 
-        # remove all redundant (after applying constraints) extrema using mask
-        mask = np.zeros((self.N * self.M))
-        mask.flat[extrema_indexes] = 1
-        extrema = mask * extrema
+        next_cand_idx = self._first_ext()  # find first extremum (max or min) and return next candidate index
+        if next_cand_idx < self.length - self.k and self.YMax.nonzero()[0]:
+            next_cand_idx = self._next_min_ext(next_cand_idx)
+        max_turn = True
+        while next_cand_idx < self.length - self.k:  # iterate through time series and find max/min one by one
+            if max_turn:
+                next_cand_idx = self._next_max_ext(next_cand_idx)
+                max_turn = False
+            else:
+                next_cand_idx = self._next_min_ext(next_cand_idx)
+                max_turn = True
 
-        self.YMin = (extrema * (extrema == 1) * 1).astype(int)
-        self.YMax = (extrema * (extrema == -1) * -1).astype(int)
+    def _first_ext(self):
+        """
+        Find first appropriate extremum and update YMin/YMax accordingly
+        :return (int): next extremum candidate index
+        """
+        idx = 0
+        min_idx = 0
+        max_idx = 0
+        while idx < self.length - self.k \
+                and (abs(self.X[idx + 1] - self.X[max_idx]) < self.D or idx <= max_idx + self.k) \
+                and (abs(self.X[idx + 1] - self.X[min_idx]) < self.D or idx <= min_idx + self.k):
+            idx += 1
+            if self.X[min_idx] > self.X[idx]:
+                min_idx = idx
+            if self.X[max_idx] < self.X[idx]:
+                max_idx = idx
+        idx += 1
+        if idx < self.length - self.k and self.X[idx] > self.X[0]:
+            self.YMin[min_idx] = 1
+        elif idx < self.length - self.k and self.X[idx] < self.X[0]:
+            self.YMax[max_idx] = 1
+        return idx
+
+    def _next_min_ext(self, idx):
+        """
+        Find next appropriate minimum and update YMin accordingly
+        :return (int): next maximum candidate index
+        """
+        min_idx = idx
+        while idx < self.length - self.k \
+                and (self.X[idx + 1] < self.X[min_idx]
+                     or idx <= min_idx + self.k
+                     or abs(self.X[idx + 1] - self.X[min_idx]) < self.D):
+            idx += 1
+            if self.X[min_idx] > self.X[idx]:
+                min_idx = idx
+        self.YMin[min_idx] = 1
+        return idx + 1
+
+    def _next_max_ext(self, idx):
+        """
+        Find next appropriate maximum and update YMax accordingly
+        :return (int): next minimum candidate index
+        """
+        max_idx = idx
+        while idx < self.length - self.k \
+                and (self.X[idx + 1] > self.X[max_idx]
+                     or idx <= max_idx + self.k
+                     or abs(self.X[idx + 1] - self.X[max_idx]) < self.D):
+            idx += 1
+            if self.X[max_idx] < self.X[idx]:
+                max_idx = idx
+        self.YMax[max_idx] = 1
+        return idx + 1
 
     def get_extrema_quantity(self):
         return sum(self.YMin) + sum(self.YMax)
 
-    def calculate_optimal_T(self, period=10, verbose=False):
+    def calculate_optimal_T(self, period=100, t_start=2, t_end=10, verbose=False):
         """
         Calculates optimal T for desired period
         :param period: Desired average period between two extrema
+        :param t_start: Start of the evaluating range for T
+        :param t_end: End of the evaluating range for T
         :param verbose: Show the plot of loss for different T
         :return: float, optimal T between 0.0 and 3.0
         """
         needed = (self.N * self.M) // period
-        iterations = 300
-        loss = np.empty((iterations))
+        iterations = (t_end - t_start) * 10
+        loss = np.empty(iterations)
         for i in range(iterations):
-            T = i / 100
+            T = t_start + (i / 10)
             self.calculate_extrema(T)
             loss[i] = ((self.get_extrema_quantity() - needed) / needed) ** 2
         if verbose:
             plt.figure(figsize=(12, 8), dpi=80)
-            plt.plot(np.array(range(iterations)) / 100, loss)
+            plt.plot(np.array(range(iterations)) / 10 + t_start, loss)
             plt.show()
-        return loss.argmin() / 100
+        return loss.argmin() / 10 + t_start
 
     def plot_random_subseries(self, quantity=4, mode='together', prediction_model=None):
         """
@@ -135,9 +195,9 @@ class syntheticSeries:
                 sub_YMin = self.YMin[idx:idx + self.N].nonzero()[0]
                 sub_YMax = self.YMax[idx:idx + self.N].nonzero()[0]
                 ax = fig.add_subplot(grid[position // 2 + 1, position % 2])
-                ax.plot(range(idx, idx + self.N),sub_X, color='g')
-                ax.scatter(sub_YMax + idx, sub_X[sub_YMax], color='r', label='Max', s=15)
-                ax.scatter(sub_YMin + idx, sub_X[sub_YMin], color='b', label='Min', s=15)
+                ax.plot(range(idx, idx + self.N),sub_X, color='g', zorder=0)
+                ax.scatter(sub_YMax + idx, sub_X[sub_YMax], color='r', label='Max', s=50, zorder=1)
+                ax.scatter(sub_YMin + idx, sub_X[sub_YMin], color='b', label='Min', s=50, zorder=1)
                 if position == 0:
                     ax.legend(loc="best", fontsize=15)
             plt.show()
@@ -154,9 +214,9 @@ class syntheticSeries:
                 sub_X = self.X[idx:idx + self.N]
                 sub_YMin = self.YMin[idx:idx + self.N].nonzero()[0]
                 sub_YMax = self.YMax[idx:idx + self.N].nonzero()[0]
-                ax1.plot(range(idx, idx + self.N), sub_X, color='g')
-                ax1.scatter(sub_YMax + idx, sub_X[sub_YMax], color='r', label='Max', s=20)
-                ax1.scatter(sub_YMin + idx, sub_X[sub_YMin], color='b', label='Min', s=20)
+                ax1.plot(range(idx, idx + self.N), sub_X, color='g', zorder=0)
+                ax1.scatter(sub_YMax + idx, sub_X[sub_YMax], color='r', label='Max', s=60, zorder=1)
+                ax1.scatter(sub_YMin + idx, sub_X[sub_YMin], color='b', label='Min', s=60, zorder=1)
                 if prediction_model is not None:
                     ax2 = fig.add_subplot(212)
                     sub_YMin_prediction, sub_YMax_prediction = prediction_model.predict_proba(sub_X)
@@ -167,59 +227,6 @@ class syntheticSeries:
         else:
             raise Exception('Unrecognized mode. Currently supported either "together" or "separate"')
 
-    def _get_extrema(self):
-        """
-        :return: numpy array, all extrema found in time series X
-        """
-        extrema = np.diff(self.X)  # calculates 1st difference for time series, which is equal to 1st derivative
-        extrema = np.sign(extrema)  # we need only sign of the derivative. Once sign is changed there is an extremum.
-        extrema = np.ediff1d(extrema, to_begin=0, to_end=0)  # ediff1d used to find change points and to add zeros
-        extrema = np.sign(extrema)  # finally -1 means Maxima, 1 means Minima
-        return extrema
-
-    def _apply_idx_constraint(self, extrema_indexes):
-        """
-        Applies first constraint (k) for extrema
-        :param extrema_indexes: array of extrema indexes
-        :return: filtered extrema
-        """
-        filtered = []
-        for idx in extrema_indexes:
-            if not filtered or idx - filtered[-1] > self.k:
-                filtered.append(idx)
-        return np.array(filtered)
-
-        # this is used in case we want to apply 2nd constraint of all points in X (not only on a consecutive one)
-
-        # def _apply_value_constraint(self, extrema_indexes):
-        #     """
-        #     Applies second constraint (D) for extrema
-        #     :param extrema_indexes: array of extrema indexes
-        #     :return: filtered extrema
-        #     """
-        #     intervals = []
-        #     extrema_indexes_filtered = []
-        #     for idx, val in enumerate(self.X[extrema_indexes]):
-        #         interval_idx = bisect.bisect_left(intervals, (val,)) - 1
-        #         if not intervals or interval_idx < 0 or val > intervals[interval_idx][1]:
-        #             intervals.insert(interval_idx + 1, (val - self.D, val + self.D))  #  add interval
-        #             extrema_indexes_filtered.append(extrema_indexes[idx])  #  add idx
-        #     return extrema_indexes_filtered
-
-    def _apply_value_constraint(self, extrema_indexes):
-        """
-        Applies second constraint (D) for extrema
-        :param extrema_indexes: array of extrema indexes
-        :return: filtered extrema
-        """
-        extrema_indexes_filtered = []
-        prev_val = None
-        for idx, val in enumerate(self.X[extrema_indexes]):
-            if prev_val is None or val < prev_val - self.D or val > prev_val + self.D:
-                prev_val = val
-                extrema_indexes_filtered.append(extrema_indexes[idx])
-        return np.array(extrema_indexes_filtered)
-
 
 if __name__ == '__main__':
     args = parse_arguments()
@@ -228,7 +235,8 @@ if __name__ == '__main__':
     T = args.T
     k = args.k
     seed_val = args.seed
-    data = syntheticSeries(N, M, k)
+
+    data = SyntheticSeries(N, M, k)
     data.generate(seed=seed_val)
     data.calculate_extrema(T)
 
